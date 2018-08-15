8c1e6abdde2aa047d68985ae0d6442b2b40225f2
Pavel <Pavel.WebOnline@gmail.com>
2018-08-15 12:22:29 +0300

    add repository

---
+++ src/main/java/com/example/bank/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.example.bank.repository;
+
+import com.example.bank.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+    Customer findById(long id);
+
+}
